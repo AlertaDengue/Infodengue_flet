5af61c4b974526eed7c23c615f7c475ae1c3c9d1
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-02-10 16:34:05 -0300

    visualizing case maps

--- geodata/wfs.py
+++ geodata/wfs.py
@@ -6,15 +6,21 @@ from owslib.wfs import WebFeatureService
 
 class InfodengueMaps:
     def __init__(self):
-        self.wfs = WebFeatureService(
+        self.wfs = None
+
+    def _connect_wfs(self):
+        return WebFeatureService(
             url="http://info.dengue.mat.br/geoserver/wfs",
             version="2.0.0"
         )
-
     def list_features(self):
+        if self.wfs is None:
+            self.wfs = self._connect_wfs()
         return list(self.wfs.contents)
 
     def get_feature(self, feature_name: str):
+        if self.wfs is None:
+            self.wfs = self._connect_wfs()
         params = dict(service='WFS', version='2.0.0', request='GetFeature',
                       typeName=feature_name, outputFormat='json')
         r = requests.get(self.wfs.url, params=params)
