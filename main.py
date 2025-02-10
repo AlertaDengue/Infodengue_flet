dd2c0b6661deb142b7a15cabd3292d425ffc50e4
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-02-10 16:08:16 -0300

    refactor: Update map visualization with Matplotlib and state-level data processing

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import flet as ft
+import pandas as pd
 from mosqlient import get_infodengue
 import datetime
 from geodata.wfs import InfodengueMaps
 import geopandas as gpd
 import plotly.express as px
+import matplotlib.pyplot as plt
 from flet.plotly_chart import PlotlyChart
+from flet.matplotlib_chart import  MatplotlibChart
 
 
 def view_sua_cidade(page: ft.Page):
@@ -18,25 +21,30 @@ def view_sua_cidade(page: ft.Page):
     )
 
 
-def view_brasil(page: ft.Page):
+def view_state(page: ft.Page):
     # Initialize Infodengue and get Brasil map
-    info_dengue = InfodengueMaps()
-    casos_por_estado = get_infodengue(
+    casos = get_infodengue(
         start_date='2022-12-30',
         end_date='2023-01-30',
         disease='dengue',
         uf='RJ'
     )
-    map_geojson = info_dengue.get_feature("RJ_distritos_2022")
-    # gdf = gpd.GeoDataFrame.from_features(map_geojson)
-    fig = px.choropleth(casos_por_estado, geojson=map_geojson, color='casos', locations='distrito')
-
+    casos = casos.groupby(['municipio_geocodigo','municipio_nome']).sum()
+    map_geojson = page.infodengue_maps.get_feature("RJ_distritos_CD2022")
+    gdf = gpd.GeoDataFrame.from_features(map_geojson)
+    gdf['CD_MUN'] = gdf['CD_MUN'].astype(int)
+    mapa = pd.merge(gdf, casos.reset_index(), left_on='CD_MUN', right_on='municipio_geocodigo', how='left')
+    fig, ax = plt.subplots()
+    mapa.plot(ax=ax,column='casos', scheme='natural_breaks',legend=True, legend_kwds={'bbox_to_anchor': (1.31, 1)})
+    ax.set_axis_off()
+    # fig = px.choropleth(mapa, geojson=mapa.geometry, color='casos', locations=mapa.index, hover_name='municipio_nome')
+    # fig.update_geos(fitbounds="locations", visible=False)
     # Create WebView to display the map
-    map_view = PlotlyChart(fig, height=500, width=500)
+    map_view = MatplotlibChart(fig, expand=True)
 
     return ft.View(
         controls=[
-            ft.Text("Mapa do Brasil", size=30, weight=ft.FontWeight.BOLD),
+            ft.Text("Mapa do Estado", size=30, weight=ft.FontWeight.BOLD),
             map_view,
             page.navigation_bar
         ],
@@ -54,12 +62,18 @@ def view_forecasts(page: ft.Page):
         scroll=ft.ScrollMode.AUTO
     )
 
+def start_map_server(page: ft.Page):
+    # Initialize Infodengue and get Brasil map
+    page.infodengue_maps = InfodengueMaps()
+
+
 
 async def main(page: ft.Page):
     page.title = "Infodengue"
     page.theme = ft.Theme(
         color_scheme_seed=ft.Colors.YELLOW,
     )
+    start_map_server(page)
     page.update()
 
     def switch_view(index):
@@ -71,7 +85,7 @@ async def main(page: ft.Page):
         elif index == 1:
             page.views.clear()
             page.views.append(
-                view_brasil(page)
+                view_state(page)
             )
 
         elif index == 2:
@@ -84,7 +98,7 @@ async def main(page: ft.Page):
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Sua cidade"),
-            ft.NavigationBarDestination(icon=ft.Icons.PUBLIC, label="Brasil"),
+            ft.NavigationBarDestination(icon=ft.Icons.PUBLIC, label="Seu Estado"),
             ft.NavigationBarDestination(icon=ft.Icons.WB_SUNNY, label="Forecasts"),
         ],
         on_change=lambda e: switch_view(e.control.selected_index)
