96e530b8aaa6b4f5deb992031e8af2f93a3988a6
Flávio Codeço Coelho <fccoelho@gmail.com>
2025-04-11 13:31:12 -0300

    refactored code to load data on demand

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from flet.plotly_chart import PlotlyChart
 from mosqlient import get_infodengue
 
 from geodata.features import InfodengueMaps, STATES
+from viz.charts import prepare_state_container, prepare_city_container
 
 
 def find_substring_matches(query: str, strings: List[str], max_results: int = 10) -> List[str]:
@@ -47,51 +48,13 @@ def select_city(page, e):
     page.go('/')
 
 
-def view_loading(page: ft.Page):
-    return ft.View(
-        route="/loading",
-        controls=[
-            page.appbar,
-            ft.Container(
-                content=ft.Column(
-                    controls=[
-                        ft.Text("Carregando dados..."),
-                        ft.ProgressBar(value=None, visible=True, width=300, height=20)
-                    ]
-                ),
-                expand=True
-            ),
-            page.navigation_bar
-        ]
-    )
-
-
-def view_main(page: ft.Page):
+def view_main(page: ft.Page, statecnt, citycnt):
     return ft.View(
         route='/',
         controls=[
             page.appbar,
-            ft.Container(
-                border=ft.border.all(1, "blue"),
-                padding=20,
-                content=ft.Column(
-                    controls=[
-                        ft.Text("Sua Cidade", size=30, weight=ft.FontWeight.BOLD),
-                        get_case_plot(page)
-                    ]),
-                expand=True
-            ),
-            ft.Container(
-                border=ft.border.all(1, "blue"),
-                padding=20,
-                content=ft.Column(
-                    controls=[
-                        ft.Text("Seu Estado", size=30, weight=ft.FontWeight.BOLD),
-                        get_state_case_map(page)
-
-                    ]),
-                expand=True
-            ),
+            citycnt,
+            statecnt,
             page.navigation_bar
             # Add more controls specific to "Infodengue"
         ],
@@ -106,73 +69,10 @@ def view_settings(page: ft.Page):
             page.appbar,
             ft.Text("Configurações", size=30),
             page.navigation_bar
-            # Add more controls specific to "Sua cidade"
+            # Add more controls specific to "Settings"
         ],
         scroll=ft.ScrollMode.AUTO
     )
-
-
-def get_state_case_map(page):
-    # Check if data is cached
-    if page.selected_state not in page.state_data_cache:
-        mapa = get_state_data(page)
-        # Cache the processed data
-        page.state_data_cache[page.selected_state] = mapa
-    else:
-        mapa = page.state_data_cache[page.selected_state]
-    fig, ax = plt.subplots()
-    mapa.plot(ax=ax,
-              column='casos',
-              scheme='natural_breaks',
-              legend=True,
-              legend_kwds={'loc': 'upper center', 'bbox_to_anchor': (0.5, -0.05), 'ncol': 2},
-              )
-    ax.set_axis_off()
-    fig.tight_layout()
-    map_view = MatplotlibChart(fig, expand=True, transparent=True, isolated=True)
-    return map_view
-
-
-def get_city_data(page):
-    """
-    Get the data for the selected city and selected disease
-    """
-    casos = get_infodengue(
-        start_date='2022-12-30',
-        end_date=date.today().isoformat(),
-        disease=page.selected_disease.lower(),
-        uf=page.selected_state,
-        geocode=page.infodengue_maps.cities[page.selected_city],
-    )
-    return casos
-
-
-def get_case_plot(page):
-    """
-    Get the plot of cases for the selected city and selected disease
-    """
-    casos = get_city_data(page)
-    fig = px.line(casos.reset_index(), x='data_iniSE', y='casos_est',
-                  title=f"Casos de {page.selected_disease} em {page.selected_city}")
-    return PlotlyChart(fig, expand=True, isolated=True)
-
-
-def get_state_data(page):
-    # Initialize Infodengue and get Brasil map
-    casos = get_infodengue(
-        start_date='2022-12-30',
-        end_date=date.today().isoformat(),
-        disease=page.selected_disease.lower(),
-        uf=page.selected_state,
-    )
-    casos = casos.groupby(['municipio_geocodigo', 'municipio_nome']).sum()
-    map_geojson = page.infodengue_maps.get_feature(f"{page.selected_state}_distritos_CD2022")
-    page.city_names = page.infodengue_maps.get_city_names()
-    gdf = gpd.GeoDataFrame.from_features(map_geojson)
-    gdf['CD_MUN'] = gdf['CD_MUN'].astype(int)
-    mapa = pd.merge(gdf, casos.reset_index(), left_on='CD_MUN', right_on='municipio_geocodigo', how='left')
-    return mapa
-
 
 async def start_map_server(page: ft.Page):
     """
@@ -184,7 +84,8 @@ async def start_map_server(page: ft.Page):
     page.city_search.options = [ft.dropdown.Option(city) for city in page.city_names]
     page.city_search.value = page.city_names[0]
     page.is_loading = False
-    page.go("/")
+    page.pr.visible = False
+    page.update()
 
 
 async def main(page: ft.Page):
@@ -195,15 +96,16 @@ async def main(page: ft.Page):
         color_scheme_seed=ft.Colors.BLUE,
     )
     page.city_names = []
-    # page.pr = ft.ProgressBar(value=None, visible=True, height=5)  # Initialize progress bar
-    # page.add(page.pr)
-    # page.update()
+    page.pr = ft.ProgressBar(value=None, visible=True, height=5)  # Initialize progress bar
+    page.add(page.pr)
+    page.update()
 
     # Initialize selected state
     page.selected_state = "RJ"
     page.selected_city = "Rio de Janeiro"
     page.selected_disease = "Dengue"
     page.state_data_cache = {}
+    page.city_data_cache = {}
     page.is_loading = True
 
     async def change_state(e):
@@ -246,7 +148,7 @@ async def main(page: ft.Page):
         if disease != page.selected_disease:
             page.selected_disease = disease
             if len(page.views) > 0 and isinstance(page.views[-1], ft.View):
-                page.go('/')  # Refresh main view
+                page.update()  # Refresh main view
 
     def create_appbar():
         disease_dropdown = ft.Dropdown(
@@ -268,7 +170,7 @@ async def main(page: ft.Page):
                 state_dropdown,
                 page.city_search,
                 disease_dropdown,
-                ft.IconButton(ft.Icons.SETTINGS),
+                ft.IconButton(ft.Icons.DRAW, tooltip= "Refresh page", on_click=lambda _: page.update()),
                 ft.IconButton(ft.Icons.HELP_OUTLINE),
             ],
             )
@@ -282,33 +184,37 @@ async def main(page: ft.Page):
         print(route, page.selected_state, page.selected_city)
         page.views.clear()
         page.views.append(
-            view_main(page)
+            ft.View(
+                route='/',
+                controls=[
+                    page.appbar,
+                    prepare_city_container(page),
+                    prepare_state_container(page),
+                    # page.navigation_bar
+                    # Add more controls specific to "Infodengue"
+                ],
+                scroll=ft.ScrollMode.AUTO
+            )
         )
-        if route == '/' and page.is_loading:
-                page.go('/loading')
-
-        elif route == '/settings':  # Settings
+        if route == '/settings':  # Settings
             page.views.append(
                 view_settings(page)
             )
-        elif route == '/loading':
-            page.views.append(
-                view_loading(page)
-            )
-
-
         page.update()
 
 
-    page.navigation_bar = ft.NavigationBar(
-        destinations=[
-            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Infodengue Report"),
-            ft.NavigationBarDestination(icon=ft.Icons.PUBLIC, label="Configurações"),
-        ],
-        on_change=lambda e: switch_view('/' if e.control.selected_index==0 else '/settings')
-    )
+    # page.navigation_bar = ft.NavigationBar(
+    #     destinations=[
+    #         ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Infodengue Report"),
+    #         ft.NavigationBarDestination(icon=ft.Icons.PUBLIC, label="Configurações"),
+    #     ],
+    #     on_change=lambda e: switch_view('/' if e.control.selected_index==0 else '/settings')
+    # )
+
 
     page.run_task(start_map_server, page)
+
+
 
 
     def view_pop(view):
---
+++ viz/charts.py
@@ -0,0 +1,126 @@
+import flet as ft
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+from flet.matplotlib_chart import MatplotlibChart
+from flet.plotly_chart import PlotlyChart
+from mosqlient import get_infodengue
+from datetime import date
+
+def _create_container(page, text=""):
+    """
+    Create a container for state selection and map display.
+    """
+    cnt = ft.Container(
+        content=ft.Column(
+            controls=[
+                ft.Text(f"{text}"),
+            ]
+        ),
+        border=ft.border.all(1, "blue"),
+        padding=20,
+        border_radius=10,
+        expand=True,
+        tooltip="Click to load data",
+    )
+    return cnt
+
+def prepare_state_container(page):
+    def get_state_data():
+        # Initialize Infodengue and get Brasil map
+        casos = get_infodengue(
+            start_date='2022-12-30',
+            end_date=date.today().isoformat(),
+            disease=page.selected_disease.lower(),
+            uf=page.selected_state,
+        )
+        casos = casos.groupby(['municipio_geocodigo', 'municipio_nome']).sum()
+        map_geojson = page.infodengue_maps.get_feature(f"{page.selected_state}_distritos_CD2022")
+        page.city_names = page.infodengue_maps.get_city_names()
+        gdf = gpd.GeoDataFrame.from_features(map_geojson)
+        gdf['CD_MUN'] = gdf['CD_MUN'].astype(int)
+        mapa = pd.merge(gdf, casos.reset_index(), left_on='CD_MUN', right_on='municipio_geocodigo', how='left')
+        page.state_data_cache[page.selected_state] = mapa
+        return mapa
+    def get_state_case_map(e):
+        """
+        Get the state case map and display it in the container.
+        :param e:
+        :return:
+        """
+        print("loading")
+        cnt.content.controls.append(pbar)
+        page.update()
+        # Check if data is cached
+        if page.selected_state not in page.state_data_cache:
+            mapa = get_state_data()
+            # Cache the processed data
+            page.state_data_cache[page.selected_state] = mapa
+        else:
+            mapa = page.state_data_cache[page.selected_state]
+        fig, ax = plt.subplots()
+        mapa.plot(ax=ax,
+                  column='casos',
+                  scheme='natural_breaks',
+                  legend=True,
+                  legend_kwds={'loc': 'upper center', 'bbox_to_anchor': (0.5, -0.05), 'ncol': 2},
+                  )
+        ax.set_axis_off()
+        fig.tight_layout()
+        map_view = MatplotlibChart(fig, expand=True, transparent=True, isolated=True)
+        cnt.content.controls.append(map_view)
+        pbar.visible = False
+        page.update()
+    cnt = _create_container(page, f"{page.selected_state}-{page.selected_city}-{page.selected_disease}")
+    pbar = ft.ProgressBar(value=None, visible=True, width=300, height=20)
+    dbutton = ft.ElevatedButton(
+        text="Baixar dados",
+        on_click=get_state_case_map,
+        icon=ft.icons.DOWNLOAD_FOR_OFFLINE,
+        tooltip="Baixa e plota os dados",
+    )
+    cnt.content.controls.append(dbutton)
+    return cnt
+
+def prepare_city_container(page):
+    def get_city_data():
+        # Initialize Infodengue and get Brasil map
+        casos = get_infodengue(
+            start_date='2022-12-30',
+            end_date=date.today().isoformat(),
+            disease=page.selected_disease.lower(),
+            uf=page.selected_state,
+            geocode=page.infodengue_maps.cities[page.selected_city],
+        )
+        return casos
+
+    def get_case_plot(e):
+        """
+        Get the plot of cases for the selected city and selected disease
+        """
+        cnt.content.controls.append(pbar)
+        page.update()
+        if page.selected_city not in page.city_data_cache:
+            casos = get_city_data()
+            page.city_data_cache[page.selected_city] = casos
+        else:
+            casos = page.city_data_cache[page.selected_city]
+
+        fig = px.line(casos.reset_index(), x='data_iniSE', y='casos_est',
+                      title=f"Casos de {page.selected_disease} em {page.selected_city}")
+        case_plot = PlotlyChart(fig, expand=True, isolated=True)
+        cnt.content.controls.append(case_plot)
+        pbar.visible = False
+        page.update()
+    cnt = _create_container(page, f"{page.selected_city}-{page.selected_disease}")
+    pbar = ft.ProgressBar(value=None, visible=True, width=300, height=10)
+    dbutton = ft.ElevatedButton(
+        text="Baixar dados",
+        on_click=get_case_plot,
+        icon=ft.icons.DOWNLOAD_FOR_OFFLINE,
+        tooltip="Baixa e plota os dados",
+    )
+    cnt.content.controls.append(dbutton)
+
+    return cnt
